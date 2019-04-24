ed814c35b854fd36fef360116dd62e352fb3d5bd
Asuka <miss1490030396@outlook.com>
2019-04-24 21:10:48 +0800

    第六章练习

---
+++ unit6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace unit6
+{
+    class Program
+    {
+        delegate double delegatepross(double param1,double param2);
+        public static double sum(double param1,double param2)=>param1+param2;
+        static void Main(string[] args)
+        {
+            delegatepross pross = new delegatepross(sum);
+            WriteLine("input number:");
+            string input = ReadLine();
+            int index = input.IndexOf(",");
+            double d1 = ToDouble(input.Substring(0,index));
+            double d2 = ToDouble(input.Substring(index+1,input.Length-index-1));
+            WriteLine($"sum = {pross(d1,d2)}");
+            ReadKey();
+        }
+    }
+}
