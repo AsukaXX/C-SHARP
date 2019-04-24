ad78a57aad013ab572545a764d63dca5cca6a3ec
Asuka <miss1490030396@outlook.com>
2019-04-24 22:43:40 +0800

    元组返回值

--- unit6/Program.cs
+++ unit6/Program.cs
@@ -6,19 +6,30 @@ namespace unit6
 {
     class Program
     {
-        delegate double delegatepross(double param1,double param2);
-        public static double sum(double param1,double param2)=>param1+param2;
-        public static void add(ref double param1,double param2){
-            param1 +=param2;
+        delegate double delegatepross(double param1, double param2);
+        public static double sum(double param1, double param2) => param1 + param2;
+        public static void add(ref double param1, double param2)
+        {
+            param1 += param2;
         }
-        public static void add1(double param1,double param2,out double outnum){
-            outnum = param1 + param2; 
+        public static void add1(double param1, double param2, out double outnum)
+        {
+            outnum = param1 + param2;
         }
-        public static double add2(params double[] darray){
+        public static (double max, double min, double sum) add2(params double[] darray)
+        {
             double sum = 0d;
-            foreach(double d in darray)
-            sum+=d;
-            return sum;
+            double max = darray[0];
+            double min = darray[0];
+            foreach (double d in darray)
+            {
+                sum += d;
+                if (d > max)
+                    max = d;
+                if (d < min)
+                    min = d;
+            }
+            return (max, min, sum);
         }
         static void Main(string[] args)
         {
@@ -26,14 +37,14 @@ namespace unit6
             WriteLine("input number:");
             string input = ReadLine();
             int index = input.IndexOf(",");
-            double d1 = ToDouble(input.Substring(0,index));
-            double d2 = ToDouble(input.Substring(index+1,input.Length-index-1));
-            add(ref d1,d2);
+            double d1 = ToDouble(input.Substring(0, index));
+            double d2 = ToDouble(input.Substring(index + 1, input.Length - index - 1));
+            add(ref d1, d2);
             WriteLine($"add, d1 = {d1}, d2 = {d2}");
-            add1(d1,d2,out double outnum);
+            add1(d1, d2, out double outnum);
             WriteLine($"add1, d1 = {d1}, d2 = {d2}, sum ={outnum}");
-            WriteLine($"add2, sum = {add2(d1,d2,outnum)}");
-            WriteLine($"pross, sum = {pross(d1,d2)}");
+            WriteLine($"add2, sum = {add2(d1, d2, outnum)}");
+            WriteLine($"pross, sum = {pross(d1, d2)}");
         }
     }
 }
