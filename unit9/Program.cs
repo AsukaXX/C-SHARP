a62eed7560d0bb971bce4d87bd5d9884feb6c0df
Asuka <miss1490030396@outlook.com>
2019-04-29 20:34:26 +0800

    第九章练习

---
+++ unit9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace unit9
+{
+    public abstract class MyBase{}
+    internal class MyClass:MyBase{}
+    public interface IMyBaseInterface{}
+    internal interface IMyBaseInterface2{}
+    internal interface IMyInterface:IMyBaseInterface,IMyBaseInterface2{}
+    internal sealed class MyComplexClass:MyClass,IMyInterface{}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyComplexClass myobj = new MyComplexClass();
+            Console.WriteLine(myobj.ToString());
+        }
+    }
+}
