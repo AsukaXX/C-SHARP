897b7d729570b6ee0b1c7e094d3d39b18acb53ca
Asuka <miss1490030396@outlook.com>
2019-04-29 21:11:27 +0800

    冒泡算法

---
+++ sort/sort3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+using static System.Array;
+
+namespace sort3
+{
+    class Program
+    {
+        static void sort(ref int[] array)
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            WriteLine("input");
+            string input = ReadLine();
+            int[] array = ConvertAll<string, int>(input.Split(","), int.Parse);
+            sort(ref array);
+            foreach (int t in array)
+                Write(t + " ");
+        }
+    }
+}
