5058fb4bf488f0b52daa2606137bb20925ebe150
Asuka <miss1490030396@outlook.com>
2019-05-06 20:53:50 +0800

    第10章练习

---
+++ learn/unit10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace unit10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Creating object myObj...");
+            MyClass myObj = new MyClass("My Object");
+            WriteLine("myObj created.");
+            for (int i = -1; i <= 0; i++)
+            {
+                try
+                {
+                    WriteLine($"\nAttempting to assign {i} to myObj.Val...");
+                    myObj.Val = i;
+                    WriteLine($"Value {myObj.Val} assigned tp myObj.Val.");
+                }
+                catch (Exception e)
+                {
+                    WriteLine($"Exception {e.GetType().FullName} thrown.");
+                    WriteLine($"Message:\n\"{e.Message}\"");
+                }
+            }
+            WriteLine("\nOutputting myObj.ToString()...");
+            WriteLine(myObj.ToString());
+            WriteLine("myObj.ToString() OutPut.");
+            WriteLine("\nmyDoubledIntPorp = 5...");
+            WriteLine($"Getting myDoubledIntProp of 5 is {myObj.myDoubledIntProp}");
+            ReadKey();
+        }
+    }
+}
