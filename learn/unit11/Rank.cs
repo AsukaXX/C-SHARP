11dabad6658d1c16cf0a89dfac4a8f5424e3f4b1
Asuka <miss1490030396@outlook.com>
2019-05-09 20:20:19 +0800

    第十章类库，后续章会用到

---
+++ learn/unit11/Rank.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace unit11
+{
+    public enum Rank
+    {
+        Ace = 1,
+        Deuce,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+    }
+}
