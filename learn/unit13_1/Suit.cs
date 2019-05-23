b8ed2cd673eba8c2c61831707b3af64d916a0443
Asuka <miss1490030396@outlook.com>
2019-05-23 21:04:48 +0800

    第十三章定制异常

---
+++ learn/unit13_1/Suit.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace unit13_1
+{
+    public enum Suit
+    {
+        Club,
+        Diamond,
+        Heart,
+        Spade,
+    }
+}
