c9e70d47008d16b70414bba5d7e05d6752373325
LIXINXIN <lixinxin@travelsky.com>
2019-06-16 17:48:04 +0800

    StartGameWindows窗口

--- learn/Karliards/KarliCards/KarliCards.Gui/GameOptions.cs
+++ learn/Karliards/KarliCards/KarliCards.Gui/GameOptions.cs
@@ -1,14 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace KarliCards.Gui
 {
     [Serializable]
     public class GameOptions
     {
-        public bool PlayAgainstComputer { get; set; }
-        public int NumberOfPlayers { get; set; }
+        private bool playAgainstComputer = true;
+        public bool PlayAgainstComputer
+        {
+            get { return playAgainstComputer; }
+            set
+            {
+                playAgainstComputer = value;
+                OnPropertyChanged(nameof(PlayAgainstComputer));
+            }
+        }
+        private int numberOfPlayers = 1;
+        public int NumberOfPlayers
+        {
+            get { return numberOfPlayers; }
+            set
+            {
+                numberOfPlayers = value;
+                OnPropertyChanged(nameof(NumberOfPlayers));
+            }
+        }
         public int MinutesBeforeLoss { get; set; }
-        public ComputerSkillLevel ComputerSkill { get; set; }
+        private ComputerSkillLevel computerSkill = ComputerSkillLevel.Dumb;
+        public ComputerSkillLevel ComputerSkill
+        {
+            get { return computerSkill; }
+            set {
+                computerSkill = value;
+                OnPropertyChanged(nameof(ComputerSkill));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private ObservableCollection<string> playerNames = new ObservableCollection<string>();
+        public List<string> SelectdPlayers { get; set; } = new List<string>();
+        public ObservableCollection<string> PlayerNames
+        {
+            get
+            {
+                return playerNames;
+            }
+            set
+            {
+                playerNames = value;
+                OnPropertyChanged("PlayerNames");
+            }
+        }
+        public void AddPlayer(string playerName)
+        {
+            if (playerNames.Contains(playerName))
+                return;
+            playerNames.Add(playerName);
+            OnPropertyChanged("PlayerNames");
+        }
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));
+        }
     }
 
     [Serializable]
