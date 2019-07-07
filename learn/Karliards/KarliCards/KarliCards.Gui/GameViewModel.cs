0ed164572bab1b18cc26312265e8b4c9f7961f2e
Asuka <asukalxx@outlook.com>
2019-07-07 19:48:24 +0800

    增加注释

--- learn/Karliards/KarliCards/KarliCards.Gui/GameViewModel.cs
+++ learn/Karliards/KarliCards/KarliCards.Gui/GameViewModel.cs
@@ -8,9 +8,11 @@ namespace KarliCards.Gui
 {
     public class GameViewModel : INotifyPropertyChanged
     {
+        //事件，属性改变
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this,
             new PropertyChangedEventArgs(propertyName));
+        //当前玩家
         private Player currentPlayer;
         public Player CurrentPlayer
         {
@@ -21,6 +23,7 @@ namespace KarliCards.Gui
                 OnPropertyChanged(nameof(CurrentPlayer));
             }
         }
+        //玩家列表
         private List<Player> players;
         public List<Player> Players
         {
@@ -31,6 +34,7 @@ namespace KarliCards.Gui
                 OnPropertyChanged(nameof(Players));
             }
         }
+        //当前玩家手牌
         private Card availableCard;
         public Card CurrentAvailableCard
         {
@@ -41,6 +45,7 @@ namespace KarliCards.Gui
                 OnPropertyChanged(nameof(CurrentAvailableCard));
             }
         }
+        //游戏牌堆
         private Deck deck;
         public Deck GameDeck
         {
@@ -51,6 +56,7 @@ namespace KarliCards.Gui
                 OnPropertyChanged(nameof(GameDeck));
             }
         }
+        //游戏状态
         private bool gameStarted;
         public bool GameStarted
         {
@@ -62,26 +68,37 @@ namespace KarliCards.Gui
             }
         }
         private GameOptions gameOptions;
+        //关联菜单中的开始游戏选项
         public static RoutedCommand StartGameCommand = new RoutedCommand("Start New Game", typeof(GameViewModel),
             new InputGestureCollection(new List<InputGesture> { new KeyGesture(Key.N, ModifierKeys.Control) }));
+        //关联菜单中的关于对话框
         public static RoutedCommand ShowAboutCommand = new RoutedCommand("Show About Dialog", typeof(GameViewModel));
         public GameViewModel()
         {
+            //创建玩家列表
             Players = new List<Player>();
+            //根据游戏选项创建游戏
             gameOptions = GameOptions.Create();
         }
+        //开始游戏
         public void StartNewGame()
         {
             if (gameOptions.SelectdPlayers.Count < 1 || (gameOptions.SelectdPlayers.Count == 1 && !gameOptions.PlayAgainstComputer))
                 return;
+            //创建新游戏牌堆
             CreateGameDeck();
+            //创建玩家
             CreatePlayers();
+            //初始化游戏
             InitalizeGame();
+            //游戏状态为true
             GameStarted = true;
         }
         private void InitalizeGame()
         {
+            //改变玩家0状态为活动状态
             AssignCurrentPlayer(0);
+            //初始化玩家手牌
             CurrentAvailableCard = GameDeck.Draw();
         }
 
@@ -94,9 +111,13 @@ namespace KarliCards.Gui
 
         private void InitalizePlayer(Player player)
         {
+            //添加玩家手牌
             player.DrawNewHand(GameDeck);
+            //事件，出牌后改变玩家为非活动状态，并且若牌堆为空，则洗牌
             player.OnCardDiscarded += player_OnCardDiscarded;
+            //事件，判断玩家是否获胜
             player.OnPlayerHasWon += player_OnPlayerHasWon;
+            //添加玩家
             Players.Add(player);
         }
 
@@ -109,6 +130,7 @@ namespace KarliCards.Gui
         {
             CurrentAvailableCard = e.Card;
             var nextIndex = CurrentPlayer.Index + 1 >= gameOptions.NumberOfPlayers ? 0 : CurrentPlayer.Index + 1;
+            //牌堆为空，则重新洗牌
             if (GameDeck.CardsInDeck == 0)
             {
                 var cardsInPlay = new List<Card>();
@@ -117,14 +139,17 @@ namespace KarliCards.Gui
                 cardsInPlay.Add(CurrentAvailableCard);
                 GameDeck.ReshuffleDiscarded(cardsInPlay);
             }
+            //改变玩家状态为活动状态
             AssignCurrentPlayer(nextIndex);
         }
 
         private void CreatePlayers()
         {
             Players.Clear();
+            //创建游戏玩家
             for (var i = 0; i < gameOptions.NumberOfPlayers; i++)
             {
+                //判断玩家是否为电脑
                 if (i < gameOptions.SelectdPlayers.Count)
                     InitalizePlayer(new Player
                     {
@@ -142,7 +167,9 @@ namespace KarliCards.Gui
 
         private void CreateGameDeck()
         {
+            //创建新的游戏牌堆
             GameDeck = new Deck();
+            //牌堆乱序
             GameDeck.Shuffle();
         }
     }
